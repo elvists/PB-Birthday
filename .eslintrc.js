4f376b17fe819a8150362af6fb34049ca74eafc0
Irlan Freitas <irlan.freitas@hotmail.com>
2023-04-07 13:16:51 -0300

    fix: layout e prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,20 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
   },
-  extends: ['plugin:react/recommended', 'standard-with-typescript'],
+  extends: ['plugin:react/recommended', 'standard-with-typescript', 'prettier'],
   overrides: [],
   parserOptions: {
     project: ['tsconfig.eslint.json', 'tsconfig.json'],
     tsconfigRootDir: __dirname,
     ecmaVersion: 'latest',
-    sourceType: 'module'
+    sourceType: 'module',
   },
   plugins: ['react'],
   rules: {
     '@typescript-eslint/explicit-function-return-type': 'off',
     'react/react-in-jsx-scope': 'off',
-    'react/prop-types': 'off'
-  }
+    'react/prop-types': 'off',
+  },
 }
