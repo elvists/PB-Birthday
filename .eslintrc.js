4ad58efb7daa739fb3bc080603c17d01df45882c
Irlan Freitas <irlan.freitas@hotmail.com>
2023-04-06 23:33:01 -0300

    feat: testando pra ver se funciona

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: ['plugin:react/recommended', 'standard-with-typescript'],
+  overrides: [],
+  parserOptions: {
+    project: ['tsconfig.eslint.json', 'tsconfig.json'],
+    tsconfigRootDir: __dirname,
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['react'],
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off'
+  }
+}
