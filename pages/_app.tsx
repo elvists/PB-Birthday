1143a2fb83aa4f29d3e40c4f49747e427494b575
Irlan Freitas <irlan.freitas@hotmail.com>
2023-04-17 15:12:13 -0300

    style: primeira versao do header
    Criacao do projeto e implementacao do layout com o basico para funcionar bem

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,74 +1,70 @@
-import React, { useState } from 'react'
-import {
-  MenuFoldOutlined,
-  MenuUnfoldOutlined,
-  UploadOutlined,
-  UserOutlined,
-  VideoCameraOutlined,
-} from '@ant-design/icons'
-import { Layout, Menu, theme } from 'antd'
+import Head from 'next/head'
+import React from 'react'
+import { Layout } from 'antd'
 import 'antd/dist/reset.css'
-import '../style.module.css'
+import 'styles.css'
+import type { AppProps } from 'next/app'
+import Image from 'next/image'
+import { LineOutlined, MoneyCollectOutlined } from '@ant-design/icons'
 
-const { Header, Sider, Content } = Layout
+const { Header, Content } = Layout
 
-const App: React.FC = () => {
-  const [collapsed, setCollapsed] = useState(false)
-  const {
-    token: { colorBgContainer },
-  } = theme.useToken()
-
+export default function App({ Component, pageProps }: AppProps): JSX.Element {
   return (
-    <Layout style={{ minHeight: '100vh' }}>
-      <Sider trigger={null} collapsible collapsed={collapsed}>
-        <div className="logo" />
-        <Menu
-          theme="dark"
-          mode="inline"
-          defaultSelectedKeys={['1']}
-          items={[
-            {
-              key: '1',
-              icon: <UserOutlined />,
-              label: 'nav 1',
-            },
-            {
-              key: '2',
-              icon: <VideoCameraOutlined />,
-              label: 'nav 2',
-            },
-            {
-              key: '3',
-              icon: <UploadOutlined />,
-              label: 'nav 3',
-            },
-          ]}
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"
         />
-      </Sider>
-      <Layout className="site-layout">
-        <Header style={{ padding: 0, background: colorBgContainer }}>
-          {React.createElement(
-            collapsed ? MenuUnfoldOutlined : MenuFoldOutlined,
-            {
-              className: 'trigger',
-              // eslint-disable-next-line @typescript-eslint/no-confusing-void-expression
-              onClick: () => setCollapsed(!collapsed),
-            }
-          )}
-        </Header>
-        <Content
-          style={{
-            margin: '24px 16px',
-            padding: 24,
-            minHeight: 280,
-            background: colorBgContainer,
-          }}
-        >
-          Content
+      </Head>
+      <Layout className="layout">
+        <Content className="content">
+          <Header className="header">
+            <div>
+              <Image
+                src="/icons/smile.svg"
+                alt="Ícone do header"
+                width={20}
+                height={20}
+                color="white"
+                style={{ marginRight: '5px' }}
+              />
+              <span>SMILE Co.</span>
+            </div>
+            <div
+              style={{ width: '100%', display: 'flex', alignItems: 'flex-end' }}
+            >
+              {/* <div> */}
+              {/* #00BBC3 - Cor padrao */}
+              <strong
+                style={{
+                  fontSize: '40px',
+                  display: 'inline-block',
+                  width: '65%',
+                  lineHeight: '1',
+                  fontStyle: 'italic',
+                }}
+              >
+                Use uma grana
+                <br /> que já é sua e<br /> saia do aperto.
+              </strong>
+              <div style={{ width: '35%', lineHeight: 2, fontSize: 10 }}>
+                <MoneyCollectOutlined />
+                <strong> SAQUE ANIVERSÁRIO</strong>
+                <br />
+                <span>
+                  <LineOutlined style={{ transform: 'rotate(90deg)' }} />
+                  <strong>Insira seus dados</strong> e verifique o<br /> quanto
+                  você poderá receber
+                </span>
+              </div>
+            </div>
+            {/* </div> */}
+          </Header>
+          <Component styles={{ background: 'green' }} {...pageProps} />
         </Content>
       </Layout>
-    </Layout>
+    </>
   )
 }
-
-export default App
